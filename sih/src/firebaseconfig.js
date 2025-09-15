cbdc20d2bec11bad841d1ac8865bf5d608c62145
sasankgit <sasank4pr@gmail.com>
2025-09-15 15:09:41 +0530

    removed firebase config and logandsign

--- sih/src/firebaseconfig.js
+++ sih/src/firebaseconfig.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBGglryEt5Zcndd6E9WUYtWr4HlZJmb9PQ",
-  authDomain: "sikkim-sih.firebaseapp.com",
-  projectId: "sikkim-sih",
-  storageBucket: "sikkim-sih.firebasestorage.app",
-  messagingSenderId: "982127278433",
-  appId: "1:982127278433:web:7f2dcdea077c92df8fed20",
-  measurementId: "G-BXTWM7P9HP"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   ///removed for privacy AKA sasank hehe
+// };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
