d9c16e0a5800d8cb6d32ca303c905b79be494881
sasankgit <sasank4pr@gmail.com>
2025-09-18 15:46:56 +0530

    re added autha and firebase

--- sih/src/App.jsx
+++ sih/src/App.jsx
@@ -7,6 +7,8 @@ import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Navbar";
 import Viewer from "./pages/Viewer";
 import SikkimMap from "./pages/SikkimMap"; 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
       <Navbar />
       <Routes>
         {/* Default Route → Dashboard */}
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/viewer" element={<Viewer />} />
         <Route path="/map" element={<SikkimMap />} /> 
--- sih/src/pages/Signup.jsx
+++ sih/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 // src/pages/Signup.jsx
 import React, { useState } from "react";
-//import { auth } from "../firebaseConfig";
-//import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
